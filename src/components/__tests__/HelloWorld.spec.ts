1825c04467418534355848787e82b82e73ac2c3b
Linnéa Ivansson (External) <linnea.ivansson@knowit.se>
2023-10-20 10:01:33 +0200

    Remove unit test

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest'
+/*import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
@@ -8,4 +8,5 @@ describe('HelloWorld', () => {
     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
-})
+})x
+*/
