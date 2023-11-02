15f4bc81a53cb36366d7288f250fc9e8d1a292c2
Linnéa Ivansson (External) <linnea.ivansson@knowit.se>
2023-11-02 13:16:42 +0100

    ci: test code review ai

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home bro</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">About this project</RouterLink>
       </nav>
     </div>
   </header>
